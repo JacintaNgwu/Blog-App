62e321a793d9a3fc9668138a68319d37bd8a4c6b
JacintaNgwu <jacintangwu9900@gmail.com>
2023-04-07 09:06:16 +0530

    Fix rubocop errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,17 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :comments
-    has_many :likes
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
 
-    after_save :update_post_counter
+  after_save :update_post_counter
 
-    def most_recent_comments
-        comments.order(created_at: :desc).limit(5)
-    end
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 
-    private
+  private
 
-    def update_post_counter
-        author.update(posts_counter: author.posts.all.count)
-    end
+  def update_post_counter
+    author.update(posts_counter: author.posts.all.count)
+  end
 end
