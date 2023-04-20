d8d372dd8d7402acc40397cde1a3383f5f65fa75
JacintaNgwu <jacintangwu9900@gmail.com>
2023-04-20 11:02:41 +0530

    Fix rubocop error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,3 @@
-# class PostsController < ApplicationController
-#   def index
-#     @posts = Post.all
-#   end
-
-#   def show
-#     @posts = Post.find(params[:id])
-#   end
-# end
-
-
 class PostsController < ApplicationController
   def index; end
 
