776cd5e66548d14dd61cfaa6d2142abb03966806
JacintaNgwu <jacintangwu9900@gmail.com>
2023-04-20 12:02:52 +0530

    Add views as per wireframe

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
 
-  def show; end
+  def show
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+    @comments = @post.comments
+  end
 end
