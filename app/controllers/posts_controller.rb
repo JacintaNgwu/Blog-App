5656b436d09346f83c6195451ce3b2df6aa43583
JacintaNgwu <jacintangwu9900@gmail.com>
2023-04-22 15:34:46 +0530

    Add forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,33 @@
 class PostsController < ApplicationController
-  def index
-    @user = User.find(params[:user_id])
-    @posts = @user.posts
-  end
+    def index
+      @user = User.find(params[:user_id])
+      @posts = @user.posts
+    end
+  
+    def new
+      @post = Post.new
+      @post.author_id = current_user.id
+    end
 
-  def show
-    @user = User.find(params[:user_id])
-    @post = Post.find(params[:id])
+    def create
+     @post = Post.new(post_params)
+     @post.author_id = current_user.id
+     respond_to do |format|
+        if @post.save
+          format.html { redirect_to user_post_path(current_user.id, @post.id), notice: 'Post was successfully created.' }
+        else
+          format.html { render :new }
+        end
+      end
+    end
+
+    def show
+      @user = User.find(params[:user_id])
+      @post = Post.find(params[:id])
+    end
+
+    private
+    def post_params
+      params.require(:post).permit(:title, :text, :comments_counter, :likes_counter)
+    end
   end
-end
