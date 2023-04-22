7ac98a95a3b8ff037cbc065ffb084f8f9d7c1cb9
JacintaNgwu <jacintangwu9900@gmail.com>
2023-04-22 15:41:45 +0530

    Fix linter error

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,26 +1,26 @@
 class LikesController < ApplicationController
-    def new
-        @like = Like.new
-        @current_user = current_user
-      end
-    
-      def create
-        @like = Like.new
-        @like.author_id = current_user.id
-        @post = Post.find(params[:post_id])
-        @like.post_id = params[:post_id]
-        respond_to do |format|
-          format.html do
-            if @like.save
-              flash[:success] = ' Like created successfully'
-              current_post = Post.find(params[:post_id])
-              @author = User.find(params[:user_id])
-              redirect_to user_post_path(@author, current_post)
-            else
-              flash[:error] = 'Something went wrong'
-              render :new
-            end
-          end
+  def new
+    @like = Like.new
+    @current_user = current_user
+  end
+
+  def create
+    @like = Like.new
+    @like.author_id = current_user.id
+    @post = Post.find(params[:post_id])
+    @like.post_id = params[:post_id]
+    respond_to do |format|
+      format.html do
+        if @like.save
+          flash[:success] = ' Like created successfully'
+          current_post = Post.find(params[:post_id])
+          @author = User.find(params[:user_id])
+          redirect_to user_post_path(@author, current_post)
+        else
+          flash[:error] = 'Something went wrong'
+          render :new
         end
+      end
     end
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,33 +1,36 @@
 class PostsController < ApplicationController
-    def index
-      @user = User.find(params[:user_id])
-      @posts = @user.posts
-    end
-  
-    def new
-      @post = Post.new
-      @post.author_id = current_user.id
-    end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
 
-    def create
-     @post = Post.new(post_params)
-     @post.author_id = current_user.id
-     respond_to do |format|
-        if @post.save
-          format.html { redirect_to user_post_path(current_user.id, @post.id), notice: 'Post was successfully created.' }
-        else
-          format.html { render :new }
+  def new
+    @post = Post.new
+    @post.author_id = current_user.id
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
+    respond_to do |format|
+      if @post.save
+        format.html do
+          redirect_to user_post_path(current_user.id, @post.id), notice: 'Post was successfully created.'
         end
+      else
+        format.html { render :new }
       end
     end
-
-    def show
-      @user = User.find(params[:user_id])
-      @post = Post.find(params[:id])
-    end
-
-    private
-    def post_params
-      params.require(:post).permit(:title, :text, :comments_counter, :likes_counter)
-    end
   end
+
+  def show
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :comments_counter, :likes_counter)
+  end
+end
