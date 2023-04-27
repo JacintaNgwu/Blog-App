84f9f34f08644513ebeee11453b178d1e09f3d0c
Juwon <dnladesanya1@gmail.com>
2023-04-28 00:36:52 +0100

    added more tests as required

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,6 +27,7 @@ require 'selenium-webdriver'
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
